b34f83831761561e2aa533328ce73676a7a8ec43
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-11-30 08:38:35 +0530

    Customer UI Design

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -4,6 +4,7 @@
  */
 package edu.ijse.mvc;
 
+import edu.ijse.mvc.view.CustomerForm;
 import edu.ijse.mvc.view.ItemView;
 
 /**
@@ -16,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new ItemView().setVisible(true);
+        new CustomerForm().setVisible(true);
     }
     
 }
